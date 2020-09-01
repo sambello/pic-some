7351ab96f651ee7224d327bb359d39cb2ebd0d46
Samuel Bello <sambello25@gmail.com>
2020-09-01 16:51:20 -0400

    Moved completed app into this repository.

---
+++ pic-some/src/components/CartItem.js
@@ -0,0 +1,24 @@
+import React, {useContext} from "react"
+import {Context} from "../Context"
+import useHover from "../hooks/useHover"
+
+export default function CartItem({item}) {
+    const [hovered, ref] = useHover()
+    const {removeFromCart} = useContext(Context)
+    
+    const iconClassName = hovered ? "ri-delete-bin-fill" : "ri-delete-bin-line"
+    
+    return (
+        <div className="cart-item">
+            <i 
+                className={iconClassName} 
+                onClick={() => removeFromCart(item.id)}
+                ref={ref}
+            >
+            </i>
+            
+            <img src={item.url} width="130px" />
+            <p>$5.99</p>
+        </div>
+    )
+}
