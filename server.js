8bb075be81557d1e19e038fae9d68b596fa6dfb5
Samuel Bello <sambello25@gmail.com>
2021-01-10 17:05:39 -0500

    Modified server.js file.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.json());
